f6b47ac14837923e561b1445cad1de2da1d17457
AramDawara123 <arram.dawara@gmail.com>
2024-12-14 01:28:28 +0100

    whatsapp toegevoegt

--- public/js/script.js
+++ public/js/script.js
@@ -6,3 +6,24 @@ document.querySelectorAll('.faq-question').forEach(question => {
     });
 });
 
+// Disable right-click and redirect
+document.addEventListener('contextmenu', function(event) {
+    event.preventDefault();
+    window.location.href = 'https://example.com/'; 
+  });
+  
+  document.addEventListener('keydown', function(event) {
+    if (event.key === 'F12') {
+      event.preventDefault();
+      window.location.href = 'https://example.com/'; 
+    }
+  
+    if ((event.ctrlKey || event.metaKey) && (event.shiftKey && event.key === 'I' || event.key === 'u')) {
+      event.preventDefault();
+      window.location.href = 'https://example.com/'; 
+    }
+  });
+  
+  if (window.navigator.userAgent.includes("extension")) {
+    window.location.href = 'https://example.com/';
+}
